05b3640d96bda69eb9e819f86781ceb47b469177
Mexbow <mohmedoksha73@gmail.com>
2024-10-07 01:27:02 +0300

    final commit inshallah

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Load models once when the app starts
 weights_path = 'weights/best14.pt'
-object_detection_model = torch.hub.load('Mexbow/yolov5_model', 'custom', path=weights_path)
+object_detection_model = torch.hub.load('Mexbow/yolov5_model', 'custom', path=weights_path, autoshape=True)
 captioning_processor = AutoImageProcessor.from_pretrained("motheecreator/ViT-GPT2-Image-Captioning")
 tokenizer = AutoTokenizer.from_pretrained("motheecreator/ViT-GPT2-Image-Captioning")
 caption_model = VisionEncoderDecoderModel.from_pretrained("motheecreator/ViT-GPT2-Image-Captioning")
@@ -22,12 +22,10 @@ def upload():
     if 'image' not in request.files:
         return redirect(request.url)
     
-    # Save uploaded image
     file = request.files['image']
     image_path = os.path.join('static', 'uploaded_image.jpg')
     file.save(image_path)
     
-    # Process image with YOLOv5 and captioning model
     img = Image.open(image_path).convert('RGB')
     results, original_caption = process_image(img)
     
@@ -35,15 +33,21 @@ def upload():
 
 def process_image(image):
     results = object_detection_model(image)
+    
+    img_with_boxes = results.render()[0]
+    detected_image_path = os.path.join('static', 'detected_image.jpg')
+    img_with_boxes = Image.fromarray(img_with_boxes)
+    img_with_boxes.save(detected_image_path)
+    
     boxes = results.xyxy[0][:, :4].cpu().numpy()
     labels = [results.names[int(x)] for x in results.xyxy[0][:, 5].cpu().numpy()]
-    
+
     # Caption the original image
     original_inputs = captioning_processor(images=image, return_tensors="pt")
     with torch.no_grad():
         caption_ids = caption_model.generate(**original_inputs)
     original_caption = tokenizer.decode(caption_ids[0], skip_special_tokens=True)
-    
+
     # Crop objects and caption them
     cropped_images = crop_objects(image, boxes)
     captions = []
@@ -54,7 +58,8 @@ def process_image(image):
         caption = tokenizer.decode(caption_ids[0], skip_special_tokens=True)
         captions.append(caption)
     
-    return {'labels': labels, 'captions': captions}, original_caption
+    return {'labels': labels, 'captions': captions, 'detected_image_path': detected_image_path}, original_caption
+
 
 def crop_objects(image, boxes):
     cropped_images = []
