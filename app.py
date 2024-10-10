99ff7d62c5bc07dfa2550b21208413e36b519998
Mohammed Abdeldayem <126143101+abdeldayem02@users.noreply.github.com>
2024-10-10 03:48:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def crop_objects(image, boxes):
         cropped_images.append(cropped_image)
     return cropped_images
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run()
